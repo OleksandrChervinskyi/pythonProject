d9c5b6a0859abfbf60c4e2277b16b2e713bebf07
OleksandrChervinskyi <OleksandrChervinskyi2@gmail.com>
2021-09-15 12:35:45 +0300

    done lesson5 - try,except,files

---
+++ django_lesson.py
@@ -0,0 +1,13 @@
+import os
+
+# print(os.uname())
+# print(os.environ)
+# print(os.system('ls'))
+# os.mkdir('new_folder')
+# print(os.path.join('111', '1.jpg')) # Будує правильні шляхи
+
+
+# manadge.py - точка входу для запуска проекту
+# asqi.py, wsqi - синхронність, асинхронність
+# settings.py - настройки всього проекту
+# url.py - реагує на урлу
